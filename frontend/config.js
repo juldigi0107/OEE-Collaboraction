f46c14770832aed71c09c05e0f1927d0d3d3dfdc
juldigi0107 <tvvvnp4rtn@privaterelay.appleid.com>
2026-09-12 22:00:30 +0700

    Update config.js

--- frontend/config.js
+++ frontend/config.js
@@ -1,1 +1,1 @@
-window.OEE_CONFIG = { apiBase: "" }; // Isi URL https://oee-collaboraction-api.<akun>.workers.dev
+window.OEE_CONFIG = { apiBase: "https://oee.offsetbmj.workers.dev" }; // Isi URL https://oee-collaboraction-api.<akun>.workers.dev
